e74284e605bb98fc87348e87cf95a96054f0a5ab
David Heller <heller1970@gmail.com>
2014-01-06 22:38:48 -0600

    detail now independent

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import csv
+import operator
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
@@ -8,6 +10,30 @@ app = Flask(__name__)
 app.config.update(
     DEBUG = True,
 )
+
+def moneyteams(file):
+    f = open(file)
+    reader = csv.reader(f)
+    reader.next()
+
+    balance = {}
+
+    for row in reader:
+        if row[3] in balance:
+            if row[2] == 'CORRECT':
+                balance[row[3]] += int(row[4])
+            else:
+                balance[row[3]] -= int(row[4])
+        else:
+            if row[2] == 'CORRECT':
+                balance[row[3]] = int(row[4])
+            else:
+                balance[row[3]] = int(row[4]) * -1
+    
+    sorted_x = sorted(balance.iteritems(), key=operator.itemgetter(1), reverse=True)
+    top3 = sorted_x[0:3]
+    bottom3 = sorted_x[-3:]
+    
 
 @app.route("/")
 
